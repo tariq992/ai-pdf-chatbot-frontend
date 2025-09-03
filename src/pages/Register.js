1e2497762e3bfad3e7454483e331b50019210834
Tariq Mehmood <tmtariq110@gmail.com>
2025-09-03 12:43:05 +0500

    Update Register.js

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -40,7 +40,7 @@ export default function Register() {
     }
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/register", {
+      const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/auth/register`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
