12457e85d255f12efae4c5d1722c66c5d26a2166
Tariq Mehmood <tmtariq110@gmail.com>
2025-09-03 12:48:01 +0500

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000"; // Change in production
+const API_BASE_URL = process.env.REACT_APP_BACKEND_URL; 
 
 const api = axios.create({
   baseURL: API_BASE_URL,
